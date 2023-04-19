4b9c79b73e463e4759575a2fd860b8e76d24952c
Cody <codybarker.or@gmail.com>
2023-04-19 14:23:34 -0700

    s

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Fozzy', 5, 'M', 'Brown', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Pigs', 6, 'M', 'Brown', 'Hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Teddy', 4, 'M', 'Brown', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Smokey', 13, 'M', 'Black', 'Type-A', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wiley', 2, 'M', 'Brown', 'Boisterous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Butter', 15, 'F', 'Brown', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Chocolate', 25, 'M', 'Black', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'Brown', 'Chill', 1);
